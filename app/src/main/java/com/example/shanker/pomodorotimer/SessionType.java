097843136537350f1b723d5b1b65c3d229113489
uday shanker <b.udayshanker@outlook.com>
2018-02-20 02:16:33 -0800

    Removed unnecessary files

--- app/src/main/java/com/example/shanker/pomodorotimer/SessionType.java
+++
@@ -1,9 +0,0 @@
-package com.example.shanker.pomodorotimer;
-
-
-
-public enum  SessionType {
-    WORK,
-    SHORT_BREAK,
-    LONG_BREAK
-}
--- app/src/main/java/com/example/shanker/pomodorotimer/TimerState.java
+++
@@ -1,8 +0,0 @@
-package com.example.shanker.pomodorotimer;
-
-
-public enum TimerState {
-    INACTIVE,
-    RUNNING,
-    PAUSE
-}
